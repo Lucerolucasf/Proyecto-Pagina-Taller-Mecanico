7285780a4a7a01f98cb105ceea6bada85189ee61
“Jhoel <nestorbertone@gmail.com>
2022-05-10 22:44:09 -0300

    Avance controlador vehiculo

--- src/main/java/com/tallereggs/controladores/VehiculoControlador.java
+++ src/main/java/com/tallereggs/controladores/VehiculoControlador.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.tallereggs.controladores;
 
-/**
- *
- * @author Admin
- */
+import com.tallereggs.enums.EnumEstado;
+import com.tallereggs.servicios.VehiculoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller 
+@RequestMapping("/")
 public class VehiculoControlador {
+    
+    @Autowired
+    private VehiculoServicio vehiculoServicio; 
+    
+    @PostMapping("/crearVehiculo")
+    public String crearVehiculo(ModelMap modelo, @RequestParam String id, @RequestParam String patente, @RequestParam String modelo, @RequestParam String marca, @RequestParam String anio, @RequestParam String km, @RequestParam String idUsuario, @RequestParam MultipartFile archivo, @RequestParam EnumEstado estado) {
+        try {
+            vehiculoServicio.crear(id, patente, modelo, marca, anio, km, idUsuario, archivo, EnumEstado.EN_ESPERA);
+            modelo.put("Su vehículo se cargó correctamente");
+        } catch (Exception ex) {
+            modelo.put("error", ex.getMessage());
+//            modelo.put("isbn", isbn);
+//            modelo.put("titulo", titulo);
+//            modelo.put("anio", anio);
+//            modelo.put("ejemplares", ejemplares);
+
+            return "index.html";
+        }
+
+        return "index.html";
+
+    }
+    
+    // public Vehiculo crear(String id, String patente, String modelo, String marca, String anio, String km, String idUsuario, MultipartFile archivo, EnumEstado estado) throws Exception {
+//        validar(patente, modelo, marca, anio, km, idUsuario, archivo, estado);
+//        Vehiculo vehiculo = new Vehiculo();
+//
+//        vehiculo.setId(id);
+//        vehiculo.setPatente(patente);
+//        vehiculo.setModelo(modelo);
+//        vehiculo.setMarca(marca);
+//        vehiculo.setAnio(anio);
+//        vehiculo.setKm(km);
+//        vehiculo.setEstado(estado);
+//        vehiculo.setUsuario(usuarioServicio.buscarPorId(idUsuario));
+//        
+//        TarjetaVerde tv = tarjetaVerdeServicio.guardar(archivo);
+//        vehiculo.setTv(tv);
+//        
+//        vehiculoRepositorio.save(vehiculo);
+//        return null;
+//
+//    }
     
 }
--- src/main/java/com/tallereggs/repositorios/VehiculoRepositorio.java
+++ src/main/java/com/tallereggs/repositorios/VehiculoRepositorio.java
@@ -5,13 +5,14 @@ import com.tallereggs.entidades.Vehiculo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehiculoRepositorio extends JpaRepository<Vehiculo, String>{
 
-    @Query("SELECT vehiculo FROM Vehiculo vheiculo WHERE vehiculo.patente = :patente")
-    public List<Vehiculo> buscarPorPatente(String patente);
+    @Query("SELECT v FROM Vehiculo v WHERE v.patente = :patente")
+    public List<Vehiculo> buscarPorPatente(@Param("patente")String patente);
     
     
     
