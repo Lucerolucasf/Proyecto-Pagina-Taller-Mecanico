99f60197be8639fec6bb3adb5a8338513c0d0d91
“Jhoel <nestorbertone@gmail.com>
2022-05-11 20:52:58 -0300

    actualizacion controladores de vehiculo

--- src/main/java/com/tallereggs/controladores/VehiculoControlador.java
+++ src/main/java/com/tallereggs/controladores/VehiculoControlador.java
@@ -1,14 +1,18 @@
 
 package com.tallereggs.controladores;
 
+import com.tallereggs.entidades.Vehiculo;
 import com.tallereggs.enums.EnumEstado;
 import com.tallereggs.servicios.VehiculoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller 
 @RequestMapping("/")
@@ -17,44 +21,54 @@ public class VehiculoControlador {
     @Autowired
     private VehiculoServicio vehiculoServicio; 
     
-    @PostMapping("/crearVehiculo")
-    public String crearVehiculo(ModelMap modelo, @RequestParam String id, @RequestParam String patente, @RequestParam String modelo, @RequestParam String marca, @RequestParam String anio, @RequestParam String km, @RequestParam String idUsuario, @RequestParam MultipartFile archivo, @RequestParam EnumEstado estado) {
+    @PostMapping("/registrarVehiculo")
+    public String registrarVehiculo(ModelMap model, @RequestParam String patente, @RequestParam String modelo, @RequestParam String marca, @RequestParam String anio, @RequestParam String km, @RequestParam String idUsuario, @RequestParam MultipartFile archivo, @RequestParam EnumEstado estado) {
         try {
-            vehiculoServicio.crear(id, patente, modelo, marca, anio, km, idUsuario, archivo, EnumEstado.EN_ESPERA);
-            modelo.put("Su vehículo se cargó correctamente");
+            vehiculoServicio.crear(patente, modelo, marca, anio, km, idUsuario, archivo, EnumEstado.EN_ESPERA);
+            
         } catch (Exception ex) {
-            modelo.put("error", ex.getMessage());
-//            modelo.put("isbn", isbn);
-//            modelo.put("titulo", titulo);
-//            modelo.put("anio", anio);
-//            modelo.put("ejemplares", ejemplares);
 
-            return "index.html";
+            model.put("error", ex.getMessage());
+            
+            model.put("patente", patente);
+            model.put("String", modelo);
+            model.put("String", marca);
+            model.put("anio", anio);
+            model.put("km", km);
+            model.put("idUsuario", idUsuario);
+            model.put("archivo", archivo);
+            model.put("estado", estado); //estado no se si iría en este método
+            
+            
+            return "registro.html"; 
         }
 
         return "index.html";
 
     }
     
-    // public Vehiculo crear(String id, String patente, String modelo, String marca, String anio, String km, String idUsuario, MultipartFile archivo, EnumEstado estado) throws Exception {
-//        validar(patente, modelo, marca, anio, km, idUsuario, archivo, estado);
-//        Vehiculo vehiculo = new Vehiculo();
-//
-//        vehiculo.setId(id);
-//        vehiculo.setPatente(patente);
-//        vehiculo.setModelo(modelo);
-//        vehiculo.setMarca(marca);
-//        vehiculo.setAnio(anio);
-//        vehiculo.setKm(km);
-//        vehiculo.setEstado(estado);
-//        vehiculo.setUsuario(usuarioServicio.buscarPorId(idUsuario));
-//        
-//        TarjetaVerde tv = tarjetaVerdeServicio.guardar(archivo);
-//        vehiculo.setTv(tv);
-//        
-//        vehiculoRepositorio.save(vehiculo);
-//        return null;
-//
-//    }
+    @PostMapping("/editarVehiculo")
+    public String editarVehiculo(RedirectAttributes attr, @RequestParam() String id, @RequestParam String patente, @RequestParam String modelo, @RequestParam String marca, @RequestParam String anio, @RequestParam String km, @RequestParam String idUsuario, @RequestParam MultipartFile archivo, @RequestParam EnumEstado estado) {
+
+        try {
+            vehiculoServicio.actualizar(id, patente, modelo, marca, anio,km, idUsuario, archivo, estado);
+            attr.addFlashAttribute("actualizado", "El vehículo se actualizó correctamente");
+
+        } catch (Exception ex) {
+            attr.addFlashAttribute("errorActualizado", ex.getMessage());
+        }
+
+        return "redirect:/vehiculos";
+    }
+    
+    @GetMapping("/vehiculos")
+    public String vehiculos(ModelMap model, RedirectAttributes attr,@RequestParam(required = false) String search) {
+        List<Vehiculo> vehiculos;
+
+        vehiculos = (List<Vehiculo>) vehiculoServicio.buscarPorPatente(search);
+        
+        return "vehiculos.html"; 
+        
+    }
     
 }
