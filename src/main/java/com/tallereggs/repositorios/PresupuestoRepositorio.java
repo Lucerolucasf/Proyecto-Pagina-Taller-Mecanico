73f12bd2205882f75489cdc07f6c74d0b000a40b
Sanchofe <santifeole@hotmail.com>
2022-05-23 19:19:44 -0300

    presusupuestoSanti

--- src/main/java/com/tallereggs/repositorios/PresupuestoRepositorio.java
+++ src/main/java/com/tallereggs/repositorios/PresupuestoRepositorio.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.tallereggs.repositorios;
 
-/**
- *
- * @author matut
- */
-public class PresupuestoRepositorio {
+import com.tallereggs.entidades.Presupuesto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PresupuestoRepositorio extends JpaRepository<Presupuesto, String> {
     
+    @Query("SELECT p FROM Presupuesto p WHERE p.id = :id")
+    public Presupuesto buscarPresupuestoPorId(@Param("id") String id);
+    
+    @Query("SELECT p FROM Presupuesto p WHERE p.usuario.nombre = :nombre")
+    public List<Presupuesto> buscarPresupuestoPorNombreUsuario(@Param("nombre") String nombre);
 }
